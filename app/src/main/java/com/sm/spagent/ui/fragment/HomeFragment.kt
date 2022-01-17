7a1e775bc9ffacd33298bd06560e844cc0bc3ed4
Monir <mknishad1@ymail.com>
2022-01-17 16:47:46 +0600

    add merchants fragment

--- app/src/main/java/com/sm/spagent/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/sm/spagent/ui/fragment/HomeFragment.kt
@@ -6,15 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.sm.spagent.R
 import com.sm.spagent.databinding.FragmentHomeBinding
 import com.sm.spagent.ui.activity.NewMerchantActivity
-import com.sm.spagent.ui.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
 
-  private lateinit var homeViewModel: HomeViewModel
   private var _binding: FragmentHomeBinding? = null
 
   // This property is only valid between onCreateView and
@@ -26,26 +22,8 @@ class HomeFragment : Fragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    homeViewModel =
-      ViewModelProvider(this)[HomeViewModel::class.java]
-
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
     val root: View = binding.root
-
-    val gridItems = listOf(
-      getString(R.string.attendance),
-      getString(R.string.exit),
-      getString(R.string.new_merchant),
-      getString(R.string.synchronize)
-    )
-    val gridIcons = listOf(
-      R.drawable.ic_attendance,
-      R.drawable.ic_exit,
-      R.drawable.ic_new_merchant,
-      R.drawable.ic_synchronize
-    )
-    //val adapter = HomeAdapter(requireContext(), gridItems, gridIcons)
-    //binding.gridView.adapter = adapter
 
     binding.newMerchantLayout.setOnClickListener {
       startActivity(Intent(requireContext(), NewMerchantActivity::class.java))
--- app/src/main/java/com/sm/spagent/ui/viewmodel/MerchantsViewModel.kt
+++ app/src/main/java/com/sm/spagent/ui/viewmodel/MerchantsViewModel.kt
@@ -1,10 +1,10 @@
 package com.sm.spagent.ui.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class MerchantsViewModel(application: Application) : BaseViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
