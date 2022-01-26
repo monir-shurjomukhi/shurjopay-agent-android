20445ac5934eb5f87b39c123a89963132b138ba5
mknishad <mknishad1@ymail.com>
2022-01-26 18:57:05 +0600

    design success activity

--- app/src/main/java/com/sm/spagent/ui/activity/SuccessActivity.kt
+++ app/src/main/java/com/sm/spagent/ui/activity/SuccessActivity.kt
@@ -2,11 +2,23 @@ package com.sm.spagent.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.sm.spagent.R
 
 class SuccessActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_success)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.setDisplayShowHomeEnabled(true)
+    supportActionBar?.title = getString(R.string.success)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // handle arrow click here
+    if (item.itemId == android.R.id.home) {
+      onBackPressed()
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
