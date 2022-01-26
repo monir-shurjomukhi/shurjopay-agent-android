cb9d926137c6eb01475f29764fc3defa93cc6a58
mknishad <mknishad1@ymail.com>
2022-01-26 17:23:22 +0600

    add success activity

---
+++ app/src/main/java/com/sm/spagent/ui/activity/SuccessActivity.kt
@@ -0,0 +1,12 @@
+package com.sm.spagent.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sm.spagent.R
+
+class SuccessActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_success)
+  }
+}
