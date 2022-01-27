c567f335d9d59a7351fb454f838c4ac2a51a2e7a
mknishad <mknishad1@ymail.com>
2022-01-27 15:37:12 +0600

    add edit personal info view model

--- app/src/main/java/com/sm/spagent/ui/activity/EditPersonalInfoActivity.kt
+++ app/src/main/java/com/sm/spagent/ui/activity/EditPersonalInfoActivity.kt
@@ -3,10 +3,18 @@ package com.sm.spagent.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sm.spagent.R
+import com.sm.spagent.databinding.ActivityEditPersonalInfoBinding
 
 class EditPersonalInfoActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityEditPersonalInfoBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_edit_personal_info)
+    binding = ActivityEditPersonalInfoBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.setDisplayShowHomeEnabled(true)
+    supportActionBar?.title = getString(R.string.edit_personal_info)
   }
 }
---
+++ app/src/main/java/com/sm/spagent/ui/viewmodel/EditPersonalInfoViewModel.kt
@@ -0,0 +1,187 @@
+package com.sm.spagent.ui.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.sm.spagent.R
+import com.sm.spagent.model.*
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+class EditPersonalInfoViewModel(application: Application) : BaseViewModel(application) {
+
+  private val _division = MutableLiveData<Division>()
+  val division: LiveData<Division>
+    get() = _division
+
+  private val _district = MutableLiveData<District>()
+  val district: LiveData<District>
+    get() = _district
+
+  private val _policeStation = MutableLiveData<PoliceStation>()
+  val policeStation: LiveData<PoliceStation>
+    get() = _policeStation
+
+  private val _ocr = MutableLiveData<Ocr>()
+  val ocr: LiveData<Ocr>
+    get() = _ocr
+
+  private val _nid = MutableLiveData<Nid>()
+  val nid: LiveData<Nid>
+    get() = _nid
+
+  private val _ownerInfo = MutableLiveData<OwnerInfo>()
+  val ownerInfo: LiveData<OwnerInfo>
+    get() = _ownerInfo
+
+  fun getDivisions() {
+    viewModelScope.launch {
+      val response = try {
+        authApiClient.getDivisions()
+      } catch (e: IOException) {
+        Log.e(TAG, "getDivisions: ${e.message}", e)
+        message.value = R.string.unable_to_connect
+        return@launch
+      } catch (e: HttpException) {
+        Log.e(TAG, "getDivisions: ${e.message}", e)
+        message.value = R.string.unable_to_connect
+        return@launch
+      }
+
+      if (response.isSuccessful && response.body() != null) {
+        _division.value = response.body()
+      } else {
+        message.value = R.string.unable_to_connect
+      }
+    }
+  }
+
+  fun getDistricts(divisionId: Int) {
+    viewModelScope.launch {
+      val response = try {
+        authApiClient.getDistricts(divisionId)
+      } catch (e: IOException) {
+        Log.e(TAG, "getDistricts: ${e.message}", e)
+        message.value = R.string.unable_to_connect
+        return@launch
+      } catch (e: HttpException) {
+        Log.e(TAG, "getDistricts: ${e.message}", e)
+        message.value = R.string.unable_to_connect
+        return@launch
+      }
+
+      if (response.isSuccessful && response.body() != null) {
+        _district.value = response.body()
+      } else {
+        message.value = R.string.unable_to_connect
+      }
+    }
+  }
+
+  fun getPoliceStations(districtId: Int) {
+    viewModelScope.launch {
+      val response = try {
+        authApiClient.getPoliceStations(districtId)
+      } catch (e: IOException) {
+        Log.e(TAG, "getPoliceStations: ${e.message}", e)
+        message.value = R.string.unable_to_connect
+        return@launch
+      } catch (e: HttpException) {
+        Log.e(TAG, "getPoliceStations: ${e.message}", e)
+        message.value = R.string.unable_to_connect
+        return@launch
+      }
+
+      if (response.isSuccessful && response.body() != null) {
+        _policeStation.value = response.body()
+      } else {
+        message.value = R.string.unable_to_connect
+      }
+    }
+  }
+
+  fun ocrNid(ocr: Ocr) {
+    viewModelScope.launch {
+      progress.value = true
+      val response = try {
+        ocrApiClient.ocrNid(ocr)
+      } catch (e: IOException) {
+        Log.e(TAG, "ocrNid: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      } catch (e: HttpException) {
+        Log.e(TAG, "ocrNid: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      }
+
+      progress.value = false
+      if (response.isSuccessful && response.body() != null) {
+        _ocr.value = response.body()
+      } else {
+        message.value = R.string.unable_to_connect
+      }
+    }
+  }
+
+  fun getNidInfo(nid: Nid) {
+    viewModelScope.launch {
+      progress.value = true
+      val response = try {
+        authApiClient.getNidInfo(nid)
+      } catch (e: IOException) {
+        Log.e(TAG, "getNidInfo: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      } catch (e: HttpException) {
+        Log.e(TAG, "getNidInfo: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      }
+
+      progress.value = false
+      if (response.isSuccessful && response.body() != null) {
+        _nid.value = response.body()
+      } else {
+        message.value = R.string.unable_to_connect_please_try_again
+      }
+    }
+  }
+
+  fun submitOwnerInfo(ownerInfo: OwnerInfo) {
+    viewModelScope.launch {
+      progress.value = true
+      val response = try {
+        authApiClient.submitOwnerInfo(ownerInfo)
+      } catch (e: IOException) {
+        Log.e(TAG, "submitOwnerInfo: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      } catch (e: HttpException) {
+        Log.e(TAG, "submitOwnerInfo: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      }
+
+      progress.value = false
+      if (response.isSuccessful && response.body() != null) {
+        _ownerInfo.value = response.body()
+      } else {
+        message.value = R.string.unable_to_connect
+      }
+    }
+  }
+
+  companion object {
+    private const val TAG = "PersonalInfoViewModel"
+  }
+}
