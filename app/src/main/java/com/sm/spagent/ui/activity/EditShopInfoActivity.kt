222a028a4775167433209cddd9fe417df83a0f03
Monir <mknishad1@ymail.com>
2022-02-01 16:22:01 +0600

    add edit shop info activity

---
+++ app/src/main/java/com/sm/spagent/ui/activity/EditShopInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.sm.spagent.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sm.spagent.R
+
+class EditShopInfoActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_edit_shop_info)
+  }
+}
