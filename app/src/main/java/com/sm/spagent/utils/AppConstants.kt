99576611028609a22516c95a83491d8a87fb577e
Rz Rasel <emailrzrasel@gmail.com>
2022-02-09 16:09:25 +0600

    Change app themes and drawable resource

--- app/src/main/java/com/sm/spagent/utils/AppConstants.kt
+++ app/src/main/java/com/sm/spagent/utils/AppConstants.kt
@@ -4,7 +4,10 @@ public class AppConstants {
     public class Http {
         companion object {
             private val baseUrl = "https://stagingapp.engine.shurjopayment.com/"
+            private val apiBaseUrl = "https://stagingapp.engine.shurjopayment.com/api/"
+            private val loginBaseUrl = "http://104.248.147.247:8000/api/v1/"
             //
+            val QR_LOGIN = "{$loginBaseUrl}qr-login/"
             val SHOP_OWNER = "{$baseUrl}shop_owner_img/"
             val SHOP_OWNER_IMAGE = "{$baseUrl}shop_owner_img/"
         }
